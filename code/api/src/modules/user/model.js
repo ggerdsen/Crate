0a028ccb9e5bafe1c78f6f7ac178b44ccb44d8d0
Corey Carter <crc2231@gmail.com>
2020-12-10 16:08:03 -0700

    Change user modules to match new columns from migration Fix duplicate entry of picture from user/model.js

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -15,8 +15,14 @@ module.exports = function(sequelize, DataTypes) {
     role: {
       type: DataTypes.TEXT
     },
+    description: {
+      type: Sequelize.TEXT
+    },
     picture: {
-      type: DataTypes.STRING
+      type: Sequelize.TEXT
+    },
+    shippingAddress: {
+      type: Sequelize.TEXT
     }
   })
 
