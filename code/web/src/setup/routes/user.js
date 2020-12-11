89de3a95dc2aae3d17171a37ebca5115012c1419
Taylor Johnson <taylorjohnson141@gmail.com>
2020-12-10 18:37:07 -0700

    Connect Edit component to path

--- code/web/src/setup/routes/user.js
+++ code/web/src/setup/routes/user.js
@@ -3,7 +3,7 @@ import Login from '../../modules/user/Login'
 import Signup from '../../modules/user/Signup'
 import Profile from '../../modules/user/Profile'
 import Subscriptions from '../../modules/user/Subscriptions'
-
+import Edit from '../../modules/user/Edit'
 // User routes
 
 // !! - Can add a route here to a new survey page after subscribing
