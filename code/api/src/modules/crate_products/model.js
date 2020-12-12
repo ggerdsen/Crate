2fe7c1b55a59c26ec789329e9bfa69892069329e
Corey Carter <crc2231@gmail.com>
2020-12-11 22:18:52 -0700

    Create module files for crate products, *Need refactored*

---
+++ code/api/src/modules/crate_products/model.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// Crate Products
+module.exports = function(sequelize, DataTypes) {
+  let CrateProduct = sequelize.define('crateProducts', {
+    userId: {
+      type: DataTypes.INTEGER
+    },
+    crateId: {
+      type: DataTypes.INTEGER
+    }
+  })
+
+  CrateProduct.associate = function(models) {
+    CrateProduct.belongsTo(models.Crate)
+    CrateProduct.belongsTo(models.Product)
+  }
+
+  return CrateProduct
+}
